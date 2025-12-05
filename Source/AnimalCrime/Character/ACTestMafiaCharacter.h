65afa4af2776c9452c2a72c3e2d5f9b0ff2eb696
sangyun lee <son5165@naver.com>
2025-12-05 17:57:26 +0900

    test: 테스트용 마피아 생성

---
+++ Source/AnimalCrime/Character/ACTestMafiaCharacter.h
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Character/ACCharacter.h"
+#include "ACTestMafiaCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ANIMALCRIME_API AACTestMafiaCharacter : public AACCharacter
+{
+	GENERATED_BODY()
+	
+
+};
