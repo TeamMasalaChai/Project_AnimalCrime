94fd1b6f5c307ec5eab6782b08bed912e64faa30
GyeonHee <hyungi4833@gmail.com>
2025-12-08 02:19:06 +0900

    커스텀 위젯 추가

--- Source/AnimalCrime/AnimalCrime.Build.cs
+++ Source/AnimalCrime/AnimalCrime.Build.cs
@@ -1,4 +1,4 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
+﻿// Copyright Epic Games, Inc. All Rights Reserved.
 
 using UnrealBuildTool;
 
@@ -12,12 +12,17 @@ public class AnimalCrime : ModuleRules
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
-		// Uncomment if you are using Slate UI
-		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
-		
-		// Uncomment if you are using online features
-		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
+        // 모듈 경로도 Include할 때 검색하도록 설정.
+        PublicIncludePaths.AddRange(new string[] {
+            "AnimalCrime"
+        });
 
-		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
-	}
+        // Uncomment if you are using Slate UI
+        PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
+
+        // Uncomment if you are using online features
+        // PrivateDependencyModuleNames.Add("OnlineSubsystem");
+
+        // To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+    }
 }
