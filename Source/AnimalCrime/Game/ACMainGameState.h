0c74307eabd65369e643f88331fcf403a9ab5e28
soloplay <somnium1108@gmail.com>
2025-12-10 20:09:16 +0900

    Add: State 관리 클래스 생성

---
+++ Source/AnimalCrime/Game/ACMainGameState.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameState.h"
+#include "ACMainGameState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ANIMALCRIME_API AACMainGameState : public AGameState
+{
+	GENERATED_BODY()
+};
