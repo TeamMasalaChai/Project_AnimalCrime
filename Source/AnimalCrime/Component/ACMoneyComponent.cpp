f530cad7e59ae1204bdc4d77551329571cabafc8
soloplay <somnium1108@gmail.com>
2025-12-18 17:32:09 +0900

    feat: Money 구조체를 가지는 Actor컴포넌트 구현
    - Component를 가지는 Actor 타입에 맞게 초기화 함수 제공
    - 돈 관리 함수 제공

--- Source/AnimalCrime/Component/ACMoneyComponent.cpp
+++ Source/AnimalCrime/Component/ACMoneyComponent.cpp
@@ -3,34 +3,154 @@
 
 #include "ACMoneyComponent.h"
 
+#include "Net/UnrealNetwork.h"
 
-// Sets default values for this component's properties
+
 UACMoneyComponent::UACMoneyComponent()
 {
-	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
-	// off to improve performance if you don't need them.
-	PrimaryComponentTick.bCanEverTick = true;
-
-	// ...
 }
 
 
-// Called when the game starts
+
 void UACMoneyComponent::BeginPlay()
 {
 	Super::BeginPlay();
-
-	// ...
-	
 }
 
+void UACMoneyComponent::InitializeComponent()
+{
+	Super::InitializeComponent();
+}
 
-// Called every frame
-void UACMoneyComponent::TickComponent(float DeltaTime, ELevelTick TickType,
-                                      FActorComponentTickFunction* ThisTickFunction)
+void UACMoneyComponent::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+	
+	DOREPLIFETIME(UACMoneyComponent, MoneyData);
+}
+
+void UACMoneyComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
-
-	// ...
 }
+
+void UACMoneyComponent::InitMoneyComponent(EMoneyType MoneyType)
+{
+	if (MoneyType == EMoneyType::None)
+	{
+		return ;
+	}
+	
+	if (MoneyData.MoneyType != EMoneyType::None)
+	{
+		return ;
+	}
+	
+	EMoneyType CurrentMoneyData = MoneyType;
+	UE_LOG(LogTemp, Error, TEXT("여긴 오니? %d"), CurrentMoneyData);
+	switch (CurrentMoneyData)
+	{
+	case EMoneyType::MoneyMafiaType:
+		InitMafiaSetting();
+		break;
+	case EMoneyType::MoneyPoliceType:
+		InitPoliceSetting();
+		break;
+	case EMoneyType::MoneyCitizenType:
+		InitCitizenSetting();
+		break;
+	default:
+		UE_LOG(LogTemp, Error, TEXT("말이 안됌."));
+	}
+}
+
+void UACMoneyComponent::EarnMoney(int32 InMoney)
+{
+	if (InMoney < 0)
+	{
+		return;
+	}
+	
+	if (MoneyData.MoneyType == EMoneyType::None)
+	{
+		return;
+	}
+	
+	// 돈 획득
+	MoneyData.Money += InMoney;
+}
+
+bool UACMoneyComponent::SpendMoney(int32 InMoney)
+{
+	if (MoneyData.MoneyType == EMoneyType::None || MoneyData.MoneyType == EMoneyType::MoneyCitizenType)
+	{
+		return false;
+	}
+	if (InMoney < 0)
+	{
+		return false;
+	}
+	if (MoneyData.Money < InMoney)
+	{
+		return false;
+	}
+	
+	// 돈 소비
+	MoneyData.Money -= InMoney;
+	
+	return true;
+}
+
+int32 UACMoneyComponent::LoseMoney(int32 InMoney)
+{
+	UE_LOG(LogTemp, Log, TEXT("[LoseMoney(int32 InMoney)]:%d vs %d"), InMoney, MoneyData.Money);
+	if (InMoney < 0)
+	{
+		return -1;
+	}
+		
+	// 크다고 리턴하면 돈이 남음.
+	if (MoneyData.Money < InMoney)
+	{
+		InMoney = MoneyData.Money;
+	}
+	
+	// 실제 값 계산
+	MoneyData.Money = FMath::Clamp(MoneyData.Money - InMoney, 0, MoneyData.Money);
+	return InMoney;
+}
+
+void UACMoneyComponent::InitMafiaSetting()
+{
+	MoneyData.MoneyType = EMoneyType::MoneyMafiaType;
+	
+	InitMoney(100);
+}
+
+void UACMoneyComponent::InitPoliceSetting()
+{
+	MoneyData.MoneyType = EMoneyType::MoneyPoliceType;
+	
+	InitMoney(200);
+}
+
+void UACMoneyComponent::InitCitizenSetting()
+{
+	MoneyData.MoneyType = EMoneyType::MoneyCitizenType;
+	
+	GenerateRandomMoney(500);
+}
+
+void UACMoneyComponent::InitMoney(int32 InMoney)
+{
+	MoneyData.Money = InMoney;
+}
+
+void UACMoneyComponent::GenerateRandomMoney(int32 InMaxMoney)
+{
+	MoneyData.Money = FMath::RandRange(0, InMaxMoney);
+	UE_LOG(LogTemp, Error, TEXT("내 돈은 얼마일까? %d"), MoneyData.Money);
+}
+
+
 
