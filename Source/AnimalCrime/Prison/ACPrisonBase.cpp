01c3a14b50270372fc858a4ce3a9ac4ba06edc6f
Sunwoo Lee <70701711+Sunnew-Lee@users.noreply.github.com>
2025-12-23 09:00:44 +0900

    feat: 감옥 구현중

---
+++ Source/AnimalCrime/Prison/ACPrisonBase.cpp
@@ -0,0 +1,120 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Prison/ACPrisonBase.h"
+#include "Character/ACCharacter.h"
+#include "Component/ACInteractableComponent.h"
+#include "Net/UnrealNetwork.h"
+#include "AnimalCrime.h"
+
+AACPrisonBase::AACPrisonBase()
+{
+	bReplicates = true;
+
+	RootComponent = CreateDefaultSubobject<USceneComponent>(TEXT("Root"));
+	InteractBoxComponent = CreateDefaultSubobject<UACInteractableComponent>(TEXT("InteractBoxComponent"));
+	InteractBoxComponent->SetupAttachment(RootComponent);
+	InteractBoxComponent->SetOffset(FVector(160.f, 0.f, 30.f));
+}
+
+void AACPrisonBase::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void AACPrisonBase::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	DOREPLIFETIME(AACPrisonBase, bDoorOpen);
+}
+
+void AACPrisonBase::OnRep_DoorOpen()
+{
+	// 클라이언트에서 문 상태 변경 이벤트 호출
+	BP_OnDoorStateChanged(bDoorOpen);
+}
+
+bool AACPrisonBase::CanInteract(AACCharacter* ACPlayer)
+{
+	if (ACPlayer == nullptr)
+	{
+		return false;
+	}
+
+	// 문이 이미 열려있으면 상호작용 불가
+	if (bDoorOpen == true)
+	{
+		return false;
+	}
+
+	if (ACPlayer->GetCharacterType() == EACCharacterType::Mafia)
+	{
+		return true;
+	}
+	else if (ACPlayer->GetCharacterType() == EACCharacterType::Police)
+	{
+		return false;
+	}
+
+	return false;
+}
+
+void AACPrisonBase::OnInteract(AACCharacter* ACPlayer)
+{
+	ShowInteractDebug(ACPlayer, GetName());
+
+	// 서버에서만 문 열기
+	if (HasAuthority())
+	{
+		OpenDoor();
+	}
+}
+
+void AACPrisonBase::OpenDoor()
+{
+	if (bDoorOpen == true)
+	{
+		return;
+	}
+
+	AC_LOG(LogSW, Log, TEXT("Prison Door Opened"));
+
+	bDoorOpen = true;
+
+	// 서버에서도 이벤트 호출 (OnRep은 클라이언트에서만 호출됨)
+	BP_OnDoorStateChanged(true);
+
+	// 자동 닫힘 타이머 설정
+	GetWorldTimerManager().SetTimer(
+		DoorTimerHandle,
+		this,
+		&AACPrisonBase::CloseDoor,
+		AutoCloseDelay,
+		false
+	);
+}
+
+void AACPrisonBase::CloseDoor()
+{
+	if (bDoorOpen == false)
+	{
+		return;
+	}
+
+	AC_LOG(LogSW, Log, TEXT("Prison Door Closed"));
+
+	bDoorOpen = false;
+
+	// 서버에서도 이벤트 호출
+	BP_OnDoorStateChanged(false);
+
+	// 타이머 클리어
+	GetWorldTimerManager().ClearTimer(DoorTimerHandle);
+}
+
+// todo: 추후에 경찰은 문의 피통 한칸당 5초로 수리?
+float AACPrisonBase::GetRequiredHoldTime() const
+{
+	return 10.0f;
+}
---
+++ Source/AnimalCrime/Prison/ACPrisonBase.h
@@ -0,0 +1,67 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Interface/ACInteractInterface.h"
+#include "ACPrisonBase.generated.h"
+
+UCLASS()
+class ANIMALCRIME_API AACPrisonBase : public AActor, public IACInteractInterface
+{
+	GENERATED_BODY()
+
+public:
+	AACPrisonBase();
+
+protected:
+	virtual void BeginPlay() override;
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+
+	UFUNCTION()
+	void OnRep_DoorOpen();
+
+protected: //!< 상호작용 인터페이스 구현
+	virtual bool CanInteract(class AACCharacter* ACPlayer) override;
+	virtual void OnInteract(class AACCharacter* ACPlayer) override;
+	virtual float GetRequiredHoldTime() const override;
+
+public:
+	/**
+		@brief 문 열기 (서버에서 호출)
+	**/
+	UFUNCTION(BlueprintCallable, Category = "Door")
+	void OpenDoor();
+
+	/**
+		@brief 문 닫기 (서버에서 호출)
+	**/
+	UFUNCTION(BlueprintCallable, Category = "Door")
+	void CloseDoor();
+
+protected:
+	/**
+		@brief 문 상태 변경 시 블루프린트에서 구현할 이벤트
+		@param bOpen - true면 열림, false면 닫힘
+	**/
+	UFUNCTION(BlueprintImplementableEvent, Category = "Door")
+	void BP_OnDoorStateChanged(bool bOpen);
+
+	//!< 상호작용 멤버변수
+protected:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Interact")
+	TObjectPtr<class UACInteractableComponent> InteractBoxComponent;
+
+	//!< 문 상태
+protected:
+	UPROPERTY(ReplicatedUsing = OnRep_DoorOpen, BlueprintReadOnly, Category = "Door")
+	bool bDoorOpen = false;
+
+	/** 문이 자동으로 닫히는 시간 (초) */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Door")
+	float AutoCloseDelay = 5.0f;
+
+private:
+	FTimerHandle DoorTimerHandle;
+};
