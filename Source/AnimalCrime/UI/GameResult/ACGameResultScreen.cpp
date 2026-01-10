abda3bc1273bd42fa50a392ca432bba3c1e8972d
sangyun lee <115783758+Sangyun1117@users.noreply.github.com>
2026-01-10 22:24:09 +0900

    style: 결과창 UI 수정

--- Source/AnimalCrime/UI/GameResult/ACGameResultScreen.cpp
+++ Source/AnimalCrime/UI/GameResult/ACGameResultScreen.cpp
@@ -2,6 +2,7 @@
 #include "UI/GameResult/ACGameResultScreen.h"
 #include "Components/TextBlock.h"
 #include "Components/HorizontalBox.h"
+#include "Components/Image.h"
 #include "ACGameResultWinnerBox.h"
 #include "Game/ACMainGameState.h"
 #include "AnimalCrime.h"
@@ -37,18 +38,44 @@ void UACGameResultScreen::SetGameResult(EGameEndType GameEndType)
 
 	if (GameEndType == EGameEndType::AllPrison || GameEndType == EGameEndType::TimeOver)
 	{
-		GameResultText->SetText(FText::FromString(TEXT("경찰 승리")));
+		GameResultText->SetText(FText::FromString(TEXT("Police Win")));
 		if(WinnerBoxClass==nullptr || WinnerBoxes==nullptr)
 		{
 			UE_LOG(LogSY, Log, TEXT("WinnerBoxClass or WinnerBoxes is nullptr"));
 			return;
 		}
 		Winners = GS->GetPlayersByRoleAndLocation(EPlayerRole::Police, ECharacterLocation::None);
+		if (PoliceWinTexture == nullptr || RoleBackGround == nullptr)
+		{
+			UE_LOG(LogSY, Log, TEXT("MafiaWinTexture or RoleBackGround is nullptr"));
+			return;
+		}
+		RoleBackGround->SetBrushFromTexture(PoliceWinTexture);
 	}
-	else
+	else if (GameEndType == EGameEndType::Escape)
 	{
-		GameResultText->SetText(FText::FromString(TEXT("마피아 승리")));
+		GameResultText->SetText(FText::FromString(TEXT("Mafia Win")));
+
+		Winners = GS->GetPlayersByRoleAndLocation(EPlayerRole::Mafia, ECharacterLocation::Escape);
+
+		if(MafiaWinTexture == nullptr || RoleBackGround == nullptr)
+		{
+			UE_LOG(LogSY, Log, TEXT("MafiaWinTexture or RoleBackGround is nullptr"));
+			return;
+		}
+		RoleBackGround->SetBrushFromTexture(MafiaWinTexture);
+	}
+	else if(GameEndType == EGameEndType::Score)
+	{
+		GameResultText->SetText(FText::FromString(TEXT("Mafia Win")));
 		Winners = GS->GetPlayersByRoleAndLocation(EPlayerRole::Mafia, ECharacterLocation::None);
+
+		if (MafiaWinTexture == nullptr || RoleBackGround == nullptr)
+		{
+			UE_LOG(LogSY, Log, TEXT("MafiaWinTexture or RoleBackGround is nullptr"));
+			return;
+		}
+		RoleBackGround->SetBrushFromTexture(MafiaWinTexture);
 	}
 
 	for (AACPlayerState* PS : Winners)
